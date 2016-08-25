52ced7d0e9503b671fface29ddafc23c0e4f8ceb
Nick Dyer <nbdyer@gmail.com>
2016-08-25 13:21:09 +0100

    Removes copy/paste error from build.gradle, and finishes tests for arguments

--- src/main/java/Arguments.java
+++ src/main/java/Arguments.java
@@ -1,15 +1,23 @@
 public class Arguments {
 
     public static int getPort(String[] args) {
-
-        int port = 5000;
-
-        for(int i = 0; i < args.length; i++) {
-            if ("-p".equals(args[i])) {
-                port = Integer.parseInt(args[i + 1]);
-            }
-        }
-        return port;
+        String port = "5000";
+        port = findFlagInArgsAndReturnValue(args, port, "-p");
+        return Integer.parseInt(port);
     }
 
+    public static String getDirectoryPath(String[] args) {
+        String directoryPath = "/Users/nickdyer/projects/cob_spec/public";
+        directoryPath = findFlagInArgsAndReturnValue(args, directoryPath, "-d");
+        return directoryPath;
+    }
+
+    private static String findFlagInArgsAndReturnValue(String[] args, String value, String flag) {
+        for(int i = 0; i < args.length; i++) {
+            if (flag.equals(args[i])) {
+                value = args[i + 1];
+            }
+        }
+        return value;
+    }
 }
--- src/test/java/ArgumentsTest.java
+++ src/test/java/ArgumentsTest.java
@@ -22,4 +22,39 @@ public class ArgumentsTest {
         assertEquals(7777, port);
     }
 
+    @Test
+    public void argumentsWillReturnTheDefaultPortOf5000() {
+        String[] arguments = new String[]{};
+
+        int port = Arguments.getPort(arguments);
+
+        assertEquals(5000, port);
+    }
+
+    @Test
+    public void argumentsCanParseADirectoryPath() {
+        String[] arguments = new String[]{"-d", "/path/to/somewhere/"};
+
+        String directoryPath = Arguments.getDirectoryPath(arguments);
+
+        assertEquals("/path/to/somewhere/", directoryPath);
+    }
+
+    @Test
+    public void argumentsCanParseADifferentDirectoryPath() {
+        String[] arguments = new String[]{"-d", "/path/to/somewhere/else/"};
+
+        String directoryPath = Arguments.getDirectoryPath(arguments);
+
+        assertEquals("/path/to/somewhere/else/", directoryPath);
+    }
+
+    @Test
+    public void argumentsWillReturnTheDefaultDirectoryToPublicCobSpec() {
+        String[] arguments = new String[]{};
+
+        String directoryPath = Arguments.getDirectoryPath(arguments);
+
+        assertEquals("/Users/nickdyer/projects/cob_spec/public", directoryPath);
+    }
 }
