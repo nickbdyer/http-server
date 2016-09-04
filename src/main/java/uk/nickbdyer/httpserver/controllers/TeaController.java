09932582dc0e5ba08b8c5a38a0de376ca24be036
Nick Dyer <nbdyer@gmail.com>
2016-09-04 13:44:27 +0100

    Adds tea controller and adds test for I'm a teapot body on coffee request

---
+++ src/main/java/uk/nickbdyer/httpserver/controllers/TeaController.java
@@ -0,0 +1,13 @@
+package uk.nickbdyer.httpserver.controllers;
+
+import uk.nickbdyer.httpserver.requests.Request;
+import uk.nickbdyer.httpserver.responses.Response;
+
+public class TeaController extends Controller {
+
+    @Override
+    public Response get(Request request) {
+        return new Response("HTTP/1.1 200 OK", "", "Short and stout");
+    }
+
+}
--- src/test/java/uk/nickbdyer/httpserver/controllers/CoffeeControllerTest.java
+++ src/test/java/uk/nickbdyer/httpserver/controllers/CoffeeControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import uk.nickbdyer.httpserver.requests.Request;
 import uk.nickbdyer.httpserver.responses.Response;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static uk.nickbdyer.httpserver.requests.Method.GET;
 
 public class CoffeeControllerTest {
@@ -17,5 +19,15 @@ public class CoffeeControllerTest {
         Response response = controller.execute(request);
 
         assertEquals("HTTP/1.1 418 OK\n", response.getStatusLine());
+    }
+
+    @Test
+    public void bodyWillContainImATeaPot() {
+        CoffeeController controller = new CoffeeController();
+        Request request = new Request(GET, "/");
+
+        Response response = controller.execute(request);
+
+        assertThat(response.getResponseBody(), containsString("I'm a teapot"));
     }
 }
