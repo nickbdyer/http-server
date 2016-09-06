68827bb33670f6c8eea5141e41b18b25f250c69b
Nick Dyer <nbdyer@gmail.com>
2016-09-06 16:53:27 +0100

    Refactor filecontroller test suite, after feedback

--- src/test/java/uk/nickbdyer/httpserver/controllers/FileControllerTest.java
+++ src/test/java/uk/nickbdyer/httpserver/controllers/FileControllerTest.java
@@ -36,7 +36,7 @@ public class FileControllerTest {
     @Test
     public void willRespondToAGetRequest() {
         FileController controller = new FileController(file);
-        Request request = new Request(GET, "/filetest");
+        Request request = getRequest("/filetest");
 
         Response response = controller.execute(request);
 
@@ -47,7 +47,7 @@ public class FileControllerTest {
     public void willReturnFileContentsInBody() throws IOException {
         Files.write(Paths.get(file.getPath()), "hello".getBytes(), StandardOpenOption.APPEND);
         FileController controller = new FileController(file);
-        Request request = new Request(GET, "/filetest");
+        Request request = getRequest("/filetest");
 
         Response response = controller.execute(request);
 
@@ -56,13 +56,10 @@ public class FileControllerTest {
     }
 
     @Test
-    public void willReturn204ForValidPatchEtag() throws IOException {
+    public void willRespond204ForPatchRequest() throws IOException {
         Files.write(Paths.get(file.getPath()), "hello".getBytes(), StandardOpenOption.APPEND);
         FileController controller = new FileController(file);
-        Map<String, String> headers = new HashMap<>();
-        headers.put("If-Match", sha1Hex(Files.readAllBytes(file.toPath())));
-        RequestLine statusLine = new RequestLine(PATCH, "/filetest", new HashMap<>());
-        Request request = new Request(statusLine, headers, "goodbye");
+        Request request = patchRequest();
 
         Response response = controller.execute(request);
 
@@ -70,17 +67,40 @@ public class FileControllerTest {
     }
 
     @Test
+    public void willRespond400ForBadPatchRequest() throws IOException {
+        Files.write(Paths.get(file.getPath()), "hello".getBytes(), StandardOpenOption.APPEND);
+        FileController controller = new FileController(file);
+        Request request = badPatchRequest();
+
+        Response response = controller.execute(request);
+
+        assertEquals(400, response.getStatusCode());
+    }
+
+    //Move to file mainpulation class when extracted
+    @Test
     public void fileWillBeUpdatedAfterValidPatchRequest() throws IOException {
         Files.write(Paths.get(file.getPath()), "hello".getBytes(), StandardOpenOption.APPEND);
         FileController controller = new FileController(file);
-        Map<String, String> headers = new HashMap<>();
-        headers.put("If-Match", sha1Hex(Files.readAllBytes(file.toPath())));
-        RequestLine statusLine = new RequestLine(PATCH, "/filetest", new HashMap<>());
-        Request request = new Request(statusLine, headers, "goodbye");
+        Request request = patchRequest();
 
         controller.execute(request);
 
         assertEquals("goodbye", new String(Files.readAllBytes(file.toPath())));
+    }
+
+    private Request getRequest(String path) {
+        return new Request(new RequestLine(GET, path, new HashMap<>()), new HashMap<>(), "");
+    }
+
+    private Request patchRequest() throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("If-Match", sha1Hex(Files.readAllBytes(file.toPath())));
+        return new Request(new RequestLine(PATCH, "/filetest", new HashMap<>()), headers, "goodbye");
+    }
+
+    private Request badPatchRequest() throws IOException {
+        return new Request(new RequestLine(PATCH, "/filetest", new HashMap<>()), new HashMap<>(), "goodbye");
     }
 
 }
