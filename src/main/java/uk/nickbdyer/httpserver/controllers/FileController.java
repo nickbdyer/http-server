5df734e8fd792d2b0fa18c4c7a1cf41e529c33b6
Nick Dyer <nbdyer@gmail.com>
2016-09-07 14:54:57 +0100

    Moves file management responsibilities to filemanager from controller, partial content working

--- src/main/java/uk/nickbdyer/httpserver/controllers/FileController.java
+++ src/main/java/uk/nickbdyer/httpserver/controllers/FileController.java
@@ -1,63 +1,45 @@
 package uk.nickbdyer.httpserver.controllers;
 
+import uk.nickbdyer.httpserver.filemanager.FileManager;
 import uk.nickbdyer.httpserver.requests.Request;
 import uk.nickbdyer.httpserver.responses.Response;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.HashMap;
-
-import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
 
 public class FileController extends Controller {
 
     private final File file;
+    private final FileManager fileMan;
 
     public FileController(File file) {
         this.file = file;
+        this.fileMan = new FileManager(file);
     }
 
     @Override
     public Response get(Request request) {
-        return new Response(200, new HashMap<>(), file);
+        if (rangeHeaderIsPresent(request)) {
+            String rangeRequestHeader = request.getHeaders().get("Range");
+            return new Response(206, fileMan.getFileHeaders(request.getHeaders()), fileMan.getFileContent(rangeRequestHeader));
+        }
+        return new Response(200, fileMan.getFileHeaders(request.getHeaders()), file);
     }
 
     @Override
     public Response patch(Request request) {
-        if (etagsMatch(request)) {
-            overwriteFileWithRequestBody(request);
+        if (fileMan.etagsMatch(request.getHeaders().getOrDefault("If-Match", ""))) {
+            fileMan.overwriteFileWith(request.getBody());
             return new Response(204, new HashMap<>(), "");
         }
         return new Response(400, new HashMap<>(), "");
     }
 
-    private boolean etagIsPresent(Request request) {
-        return request.getHeaders().containsKey("If-Match");
+
+    private boolean rangeHeaderIsPresent(Request request) {
+        return request.getHeaders().containsKey("Range");
     }
 
-    private boolean etagsMatch(Request request) {
-        return etagIsPresent(request) && request.getHeaders().get("If-Match").equals(fileSha(file));
-    }
 
-    private void overwriteFileWithRequestBody(Request request) {
-        try {
-            FileWriter writer = new FileWriter(file, false);
-            writer.write(request.getBody());
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String fileSha(File file) {
-        try {
-            return sha1Hex(Files.readAllBytes(file.toPath()));
-        } catch (IOException e) {
-            return null;
-        }
-
-    }
 
 }
--- src/main/java/uk/nickbdyer/httpserver/filemanager/FileManager.java
+++ src/main/java/uk/nickbdyer/httpserver/filemanager/FileManager.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
@@ -11,9 +13,11 @@ import static org.apache.commons.codec.digest.DigestUtils.sha1Hex;
 public class FileManager {
 
     private final File file;
+    private final byte[] fileBytes;
 
     public FileManager(File file) {
         this.file = file;
+        this.fileBytes = readFile(file);
     }
 
     public void overwriteFileWith(String newContent) {
@@ -30,13 +34,37 @@ public class FileManager {
         return !Objects.equals("", etag) && etag.equals(fileSha(file));
     }
 
+    public Map<String, String> getFileHeaders(Map<String, String> requestHeaders) {
+        Map<String, String> responseHeaders = new HashMap<>();
+        if (requestHeaders.containsKey("Range")) {
+            RangeFinder rangeFinder = new RangeFinder(requestHeaders.get("Range"), fileBytes.length);
+            responseHeaders.put("Content-Range", "bytes " + rangeFinder.getLowerBound() + "-" + rangeFinder.getUpperBound() + "/" + fileBytes.length);
+        }
+        return responseHeaders;
+    }
+
+    public String getFileContent(String range) {
+        RangeFinder rangeFinder = new RangeFinder(range, fileBytes.length);
+        return new String(fileBytes).substring(rangeFinder.getLowerBound(), rangeFinder.getUpperBound() + 1);
+    }
+
+    private byte[] readFile(File file) {
+        byte[] fileBytes = "".getBytes();
+        try {
+            fileBytes = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileBytes;
+    }
+
     private String fileSha(File file) {
         try {
             return sha1Hex(Files.readAllBytes(file.toPath()));
         } catch (IOException e) {
             return null;
         }
-
     }
+
 
 }
