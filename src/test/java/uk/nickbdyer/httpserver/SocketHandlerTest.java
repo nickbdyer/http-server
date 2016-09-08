248054b1849da500fd104ce2a7ba819731118c0e
Nick Dyer <nbdyer@gmail.com>
2016-09-08 11:07:11 +0100

    Adds test for uncheckedioexception from socket inputstream

--- src/test/java/uk/nickbdyer/httpserver/SocketHandlerTest.java
+++ src/test/java/uk/nickbdyer/httpserver/SocketHandlerTest.java
@@ -3,9 +3,11 @@ package uk.nickbdyer.httpserver;
 import org.junit.Test;
 import uk.nickbdyer.httpserver.testdoubles.DummyLogger;
 import uk.nickbdyer.httpserver.testdoubles.SocketStub;
+import uk.nickbdyer.httpserver.testdoubles.UnreadableSocketStub;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.UncheckedIOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
@@ -21,5 +23,12 @@ public class SocketHandlerTest {
         socketHandler.processRequestAndRespond();
 
         assertThat(recievedResponse.toString(), containsString("HTTP/1.1 404 Not Found"));
+    }
+
+    @Test(expected = UncheckedIOException.class)
+    public void socketHandlerWillThrowUncheckIOExceptionIfStreamsCannotBeRead() {
+        SocketHandler socketHandler = new SocketHandler(new UnreadableSocketStub(), new DummyLogger(), new Router(new File("")));
+
+        socketHandler.processRequestAndRespond();
     }
 }
